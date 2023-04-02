e501ae81808fd1868c0a2ae04e84555af1b73e9a
Ethan Hill <ethan.hill@drummondcentral.co.uk>
2023-04-02 11:55:29 +0100

    Favicon and meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,12 +8,12 @@
 
 <script setup lang="ts">
 useServerSeoMeta({
-  title: "My Amazing Site",
-  ogTitle: "My Amazing Site",
-  description: "This is my amazing site, let me tell you all about it.",
-  ogDescription: "This is my amazing site, let me tell you all about it.",
-  ogImage: "https://example.com/image.png",
-  twitterCard: "summary_large_image",
+  title: "Ethan Hill | Portfolio",
+  ogTitle: "Ethan Hill | Portfolio",
+  description:
+    "This is my new portfolio site showing off my recent work, progress and about me.",
+  ogDescription:
+    "This is my new portfolio site showing off my recent work, progress and about me.",
 });
 
 useCookie("tipShown", {
